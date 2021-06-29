3b937b3e57f3e7be9ab325ddb93bf548a5fb228e
clava4 <slavik.kulynych@gmail.com>
2021-06-29 21:28:27 +0300

    Performed todo.

--- src/Encrypting.java
+++ src/Encrypting.java
@@ -2,7 +2,7 @@ public class Encrypting {
 
     public static void main(String[] args) {
         final CaesarCipher encrypt = new CaesarCipher();
-        String encryptedMessage = encrypt.encryption("Hello World! I'm Slavik.", 7);
+        String encryptedMessage = encrypt.encrypt("Hello World! I'm Slavik.", 5465);
         System.out.println(encryptedMessage);
     }
 }
